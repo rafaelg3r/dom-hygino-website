1f31efdc0d8c33233ab6824cc0461cb2aaa26e02
Rafael da Costa Vedana <rafael.vedana@outlook.com>
2025-08-11 04:35:07 -0300

    add navbar expand element on hover + burguer click

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
+const headerProducts = document.querySelector(".nav-produtos-container")
+const headerProductsList = document.querySelector(".produtos-list-wrapper")
+
+// const burguerProducts = document.querySelector(".burguer-produtos")
+// const burguerProductsList = document.querySelector(
+//   ".burguer-produtos-list-wrapper"
+// )
+
+const burguerMenu = document.getElementById("header-options")
+const burguerMenuList = document.querySelector(".burguer-options")
+
+headerProducts.addEventListener("mouseenter", () => {
+  headerProductsList.classList.remove("hidden")
+})
+
+headerProducts.addEventListener("mouseleave", () => {
+  headerProductsList.classList.add("hidden")
+})
+
+burguerMenu.addEventListener("click", () => {
+  burguerMenuList.classList.toggle("hidden")
+})
+
+document.addEventListener("DOMContentLoaded", () => {
   new Splide("#my-carousel", {
     type: "loop",
     perPage: 1,
