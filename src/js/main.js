9d80be9fdd36bdd436f03fe97a22d71fd4f98cb5
Rafael da Costa Vedana <rafael.vedana@outlook.com>
2025-08-12 05:52:27 -0300

    menu started + media queries fixes + footer fixes + header fixes

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const headerProductsList = document.querySelector(".produtos-list-wrapper")
 
 const burguerMenu = document.getElementById("header-options")
 const burguerMenuList = document.querySelector(".burguer-options")
+const burguerMenuItem = document.querySelectorAll(".burguer-option")
 
 headerProducts.addEventListener("mouseenter", () => {
   headerProductsList.classList.remove("hidden")
@@ -19,6 +20,12 @@ headerProducts.addEventListener("mouseleave", () => {
 
 burguerMenu.addEventListener("click", () => {
   burguerMenuList.classList.toggle("hidden")
+})
+
+burguerMenuItem.forEach(item => {
+  item.addEventListener("click", () => {
+    burguerMenuList.classList.add("hidden")
+  })
 })
 
 document.addEventListener("DOMContentLoaded", () => {
